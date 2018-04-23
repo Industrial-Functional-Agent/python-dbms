f94790d27e3fbcde9b635237b6c501f95ff1ae9c
Changhyun Lee <chang12@snu.ac.kr>
2018-04-23 21:45:17 +0900

    parse_tree 함수에 syntax 바뀐것 반영

--- utils.py
+++ utils.py
@@ -2,5 +2,10 @@ from models import CreateTable
 
 
 def parse_tree(tree):
-    if tree.data == "create":
-        return CreateTable.parse_create(tree)
+    if tree.data == "sql":
+        t = tree.children[0]
+        if t.data == "ddl":
+            t = t.children[0]
+            if t.data == "create_table":
+                return CreateTable.parse_create(t)
+    raise RuntimeWarning("invalid tree {}".format(tree))
